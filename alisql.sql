ec26a331922e777604d4c184f70a2853cfd879f3
songchuan.zhou <songchuan.zhou@alibaba-inc.com>
2017-07-03 22:28:27 +0800

    增加sql

---
+++ alisql.sql
@@ -0,0 +1,27 @@
+  select
+  sum(case when content like "% 403 %" then 1 else 0 end) as status_403,
+  count(*) as total
+  from autonavi_aos_dw.s_tt_nginx_amap_aos_tt4
+  where ds=20170702 and hh=05
+  and (content like "%02/Jul/2017:05:11:%"
+  or content like "%02/Jul/2017:05:12:%"
+  or content like "%02/Jul/2017:05:13:%"
+  or content like "%02/Jul/2017:05:14:%"
+  or content like "%02/Jul/2017:05:15:%"
+  or content like "%02/Jul/2017:05:16:%"
+  or content like "%02/Jul/2017:05:17:%"
+  or content like "%02/Jul/2017:05:18:%"
+  or content like "%02/Jul/2017:05:19:%"
+  or content like "%02/Jul/2017:05:20:%"
+  or content like "%02/Jul/2017:05:21:%"
+  or content like "%02/Jul/2017:05:22:%"
+  or content like "%02/Jul/2017:05:23:%"
+  or content like "%02/Jul/2017:05:24:%"
+  or content like "%02/Jul/2017:05:25:%");
+
+
+  +------------+------------+
+  | status_403 | total      |
+  +------------+------------+
+  | 267704801  | 367674080  |
+  +------------+------------+
